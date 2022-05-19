6d1e03359dbc42b6c6958cfbeb3d309ffc58da5c
AprilYuge <yuge.wang@yale.edu>
2022-05-19 16:24:33 -0400

    corrected typos in setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ with open("./README.md", "r", encoding='utf-8') as fh:
 setuptools.setup(
     name="ResPAN",
     version="0.1.0",
-    keywords=['scRNA-seq', 'GAN', 'batch effect correction', 'neural network']
+    keywords=['scRNA-seq', 'GAN', 'batch effect correction', 'neural network'],
     author="Yuge Wang, Tianyu Liu",
     author_email="wangyuge22@qq.com",
     description="A light structured residual autoencoder and mutual nearest neighbor paring guided adversarial network for scRNA-seq batch correction. ",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    license='MIT License'
+    license='MIT License',
     url="https://github.com/AprilYuge/ResPAN",
     install_requires = [
     'numpy',
